acdade568ff243bfea123583f328c2821be85399
arizdanial <ariz.danial23@gmail.com>
2021-07-07 21:35:05 +0800

    ~ added log in feature. created new bp session. Log in with username, password/hashing. Sign out feature also added in bp session. Signout button made on home.html of templates root folder.

---
+++ instagram_web/blueprints/sessions/views.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from models.user import User
+from werkzeug.security import check_password_hash
+from flask_login import login_user, logout_user
+
+sessions_blueprint = Blueprint('sessions',
+                               __name__,
+                               template_folder='templates')
+
+
+@sessions_blueprint.route("/new")
+def new():
+    return render_template("sessions/new.html")
+
+
+@sessions_blueprint.route("/", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = User.get_or_none(User.username == username)
+    if user and check_password_hash(user.password_hash, password):
+        flash("Login successful!")
+        login_user(user)
+        return redirect(url_for('home'))
+    else:
+        print("Bad login")
+        return redirect(url_for('sessions.new'))
+
+
+@sessions_blueprint.route("/delete", methods=["POST"])
+def destroy():
+    logout_user()
+    return redirect(url_for('sessions.new'))
